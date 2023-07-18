fb3abacab4e974ce075c34e46e2f981c62bce2c8
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2023-07-18 17:57:46 +0400

    initial commit

---
+++ basics.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer, {
+  cors: {
+    origin: '*', // Allow connections from any origin (replace with your specific domain in a production environment)
+    methods: ['GET', 'POST'], // Allow specified HTTP methods
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id, 'connected');
+  // in ws we use send method and in socket.io we use emit event
+  socket.emit('messageFromServer', { data: 'Welcome to the socketio server' });
+});
